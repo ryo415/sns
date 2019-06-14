aae4e377dac80e8def96b58116c645047a328e5e
ryo415 <black_in7@hotmail.co.jp>
2019-06-14 18:00:41 +0900

    get info from postgres

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bcrypt = require('bcrypt');
 var fs = require('fs');
 var bodyParser = require('body-parser');
+var { Client } = require('pg');
 const saltRounds = 10;
 var app = express();
 
@@ -9,6 +10,14 @@ app.use(bodyParser.urlencoded({
 	extended: true
 }));
 app.use(bodyParser.json());
+
+const client = new Client({
+	user: 'postgres',
+	host: 'localhost',
+	database: 'sns',
+	password: 'postgres',
+	port: 5432
+})
 
 var server = app.listen(3000, function(){
 	console.log("Node.js is listening to PORT:" + server.address().port);
@@ -30,27 +39,43 @@ function get_passwd_hash(id) {
 	return -1
 }
 
-
 app.set('view engine', 'ejs');
 
 app.get("/", function(req, res, next) {
 	res.render("index", {});
 });
 
-app.post("/login", function(req, res, next) {
-	var input_userid = req.body.userid;
-	var input_passwd = req.body.passwd;
-	var input_passwd_hash;
-	var db_passwd_hash = get_passwd_hash(input_userid);
+app.post("/login", (req, res, next) => {
+	(async () => {
+		var input_userid = req.body.userid;
+		var input_passwd = req.body.passwd;
+		var input_passwd_hash;
+		//var db_passwd_hash = get_passwd_hash(input_userid);
+		var db_passwd_hash;
+	
+		var query = "select passwd from member where id='" + input_userid + "'";
+		await client.connect();
+		var result = await client.query(query);
+		await client.end();
 
-	if(db_passwd_hash != -1) {
-		if(bcrypt.compareSync(input_passwd, db_passwd_hash)) {
-			res.send("good login");
+		if(typeof result.rows[0] !== 'undefined') {
+			db_passwd_hash = result.rows[0].passwd;
 		} else {
-			res.send("bad login");
-
+			db_passwd_hash = -1;
 		}
-	} else {
-		res.send("no user");
-	}
+		
+		console.log(db_passwd_hash);
+		console.log(input_passwd);
+
+		if(db_passwd_hash != -1) {
+			if(bcrypt.compareSync(input_passwd, db_passwd_hash)) {
+				res.send("good login");
+			} else {
+				res.send("bad login");
+	
+			}
+		} else {
+			res.send("no user");
+		}
+	})().catch(next);
 });
